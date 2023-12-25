774a720b73b7f3ec3e4eabcda73b205dbbc9586a
oshita <oshita@cv.info.gifu-u.ac.jp>
2023-12-25 17:11:16 +0900

    initial commit

--- scripts/generate_mvtec.py
+++ scripts/generate_mvtec.py
@@ -4,8 +4,8 @@ from mirage_defect.mask_generator import UniformMaskGenerator, GroundendSamMaskG
 from mirage_defect.nsa_generator import NsaGenerator, NsaGeneratorArgs
 
 
-DATASET_ROOT = "/dataset/mvtec"
-DST_DIR = "/dataset/mvtec_nsa"
+DATASET_ROOT = "/home/dataset/mvtec"
+DST_DIR = "/home/dataset/mvtec_nsa"
 N = 3
 
 OBJECT_CATEGORIES = [
@@ -31,21 +31,23 @@ TEXTURE_CATEGORIES = [
 
 CATEGORIES = OBJECT_CATEGORIES + TEXTURE_CATEGORIES
 
-
 for category in CATEGORIES:
     mask_generator = UniformMaskGenerator() if category in TEXTURE_CATEGORIES else GroundendSamMaskGenerator()
     generator = NsaGenerator(NsaGeneratorArgs(mode="uniform", shift=True, resize=True, num_patches=3), mask_generator)
 
     dir_path = Path(DATASET_ROOT) / category / "train" / "good"
-
+    # import pdb;pdb.set_trace()
     for path in dir_path.iterdir():
         im = Image.open(path)
         caption = category
 
         outputs = generator.generate(im, im, caption, n=N)
-        for idx, (x, mask) in enumerate(zip(*outputs)):
-            dst_image_path = Path(DST_DIR) / category / "images" / f"{path.stem}_{idx}.bmp"
-            dst_mask_path = Path(DST_DIR) / category / "masks" / f"{path.stem}_{idx}.bmp"
+        # import pdb;pdb.set_trace()
+        outputs = [(outputs[0][i], outputs[0][i + 1]) for i in range(0, len(outputs[0]), 2)]
+        # import pdb;pdb.set_trace()
+        for idx, (x, mask) in enumerate(outputs):
+            dst_image_path = Path(DST_DIR) / category / "images" / f"{path.stem}_{idx}.jpg"
+            dst_mask_path = Path(DST_DIR) / category / "masks" / f"{path.stem}_{idx}.jpg"
 
             dst_image_path.parent.mkdir(exist_ok=True, parents=True)
             dst_mask_path.parent.mkdir(exist_ok=True, parents=True)
